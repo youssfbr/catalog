caf7fb3946ff66d1a355eab43a5bcacffd19a650
alisson youssf <youssfbr@gmail.com>
2020-09-28 20:34:29 -0300

    Database seeding

--- backend/src/main/java/com/alissondev/catalog/services/CategoryService.java
+++ backend/src/main/java/com/alissondev/catalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alissondev.catalog.entities.Category;
 import com.alissondev.catalog.repositories.CategoryRepository;
@@ -14,8 +15,8 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
-	public List<Category> findAll() {
-		
+	@Transactional(readOnly = true)
+	public List<Category> findAll() {		
 		return repository.findAll();
 	}
 }
