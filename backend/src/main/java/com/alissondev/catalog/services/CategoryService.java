6a7425d7b70e278f96bd32f04aa1f5e178070b3f
alisson youssf <youssfbr@gmail.com>
2020-09-30 11:37:36 -0300

    Auditing data

--- backend/src/main/java/com/alissondev/catalog/services/CategoryService.java
+++ backend/src/main/java/com/alissondev/catalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.alissondev.catalog.dto.CategoryDTO;
 import com.alissondev.catalog.entities.Category;
 import com.alissondev.catalog.repositories.CategoryRepository;
+import com.alissondev.catalog.services.exceptions.DatabaseException;
 import com.alissondev.catalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -52,5 +55,19 @@ public class CategoryService {
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}		
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);	
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
+		
 	}
 }
